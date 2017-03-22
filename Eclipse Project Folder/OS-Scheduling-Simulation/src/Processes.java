3053e14421e21da153d08bd0cd09d11c607276bd
Alex Blair <ablair18@my.whitworth.edu>
2017-03-22 16:52:01 -0700

    Updated Eclipse Project with Java Files

---
+++ Eclipse Project Folder/OS-Scheduling-Simulation/src/Processes.java
@@ -0,0 +1,34 @@
+public class Processes {
+	int time;
+	String name;
+	int waitTime;
+	int turnaround;
+	int response;
+	
+	int getwaitTime(){return waitTime;}
+	int getTime(){return time;}
+	String getName(){return name;}
+	int getTurnaround(){return turnaround;}
+	int getResponse(){return response;}
+	
+	void setTime( int t){
+		time = t;
+	}
+	void setName(String n){
+		name = n;
+	}
+	void setWaitTime(int w){
+		waitTime =w;
+	}
+	void setTurnaround(int t){
+		turnaround = t;
+	}
+	void setResponse(int r){
+		response=r;
+	}
+	
+	public Processes(int t, String n){
+		time = t;
+		name = n;
+	}
+}
